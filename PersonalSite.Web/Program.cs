45d02c92441cc05f4ee72d8550aec1b4fe3ebad5
herorelative <herorelative@gmail.com>
2023-05-30 20:30:43 +0630

    feat: contact page and shadow

--- PersonalSite.Web/Program.cs
+++ PersonalSite.Web/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PersonalSite.Web;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.Configure<StaticFileOptions>(options =>
+{
+    options.OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.TryAdd("Cache-Control", "public,max-age=2592000");
+        ctx.Context.Response.Headers.TryAdd("Expires", DateTime.UtcNow.AddDays(30).ToString("R", CultureInfo.InvariantCulture));
+    };
+});
 
 await builder.Build().RunAsync();
